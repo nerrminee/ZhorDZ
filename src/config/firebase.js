bb100158671e3356ca5220eca47b8c7a40c923ee
bnc_nermine <nerminebennaci5@gmail.com>
2026-05-29 02:59:23 +0200

    config fire base

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,26 +1,19 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyACHy5lbmVR82TE_nhXXwJTsHmFY1lfLPs",
+  authDomain: "zhor-d0b03.firebaseapp.com",
+  projectId: "zhor-d0b03",
+  storageBucket: "zhor-d0b03.firebasestorage.app",
+  messagingSenderId: "107527212861",
+  appId: "1:107527212861:web:e8833652eb0a2755c6bf54"
 }
 
-const hasFirebaseConfig = Object.values(firebaseConfig).every(Boolean)
+const app = initializeApp(firebaseConfig)
 
-export const firebaseApp = hasFirebaseConfig
-  ? initializeApp(firebaseConfig)
-  : null
+export const auth = getAuth(app)
+export const db = getFirestore(app)
 
-export const auth = firebaseApp ? getAuth(firebaseApp) : null
-export const db = firebaseApp ? getFirestore(firebaseApp) : null
-export const storage = firebaseApp ? getStorage(firebaseApp) : null
-export const isFirebaseConfigured = hasFirebaseConfig
-
-export default firebaseApp
+export default app
